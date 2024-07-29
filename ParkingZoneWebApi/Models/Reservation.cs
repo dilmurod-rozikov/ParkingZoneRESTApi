95c7824737a6f93e9e066f4444f56ad2df7dd461
Dilmurod Rozikov <dilmurodrozikovvv@gmail.com>
2024-07-29 11:24:50 +0500

    Finish models and create database tables

---
+++ ParkingZoneWebApi/Models/Reservation.cs
@@ -0,0 +1,30 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace ParkingZoneWebApi.Models
+{
+    [Table("Reservation")]
+    public class Reservation
+    {
+        [Required]
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        public uint Duration { get; set; }
+
+        [Required]
+        public DateTime Started { get; set; }
+
+        [Required]
+        [MaxLength(10)]
+        public string VehicleNO { get; set; }
+
+        [Required]
+        [ForeignKey(nameof(ParkingSlot))]
+        public int ParkingSlotId { get; set; }
+
+        [Required]
+        public ParkingSlot ParkingSlot { get; set; }
+    }
+}
--- ParkingZoneWebApi/Program.cs
+++ ParkingZoneWebApi/Program.cs
@@ -1,4 +1,7 @@
 
+using Microsoft.EntityFrameworkCore;
+using ParkingZoneWebApi.DataAccess;
+
 namespace ParkingZoneWebApi
 {
     public class Program
@@ -7,13 +10,11 @@ namespace ParkingZoneWebApi
         {
             var builder = WebApplication.CreateBuilder(args);
 
-            // Add services to the container.
-
             builder.Services.AddControllers();
-            // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
             builder.Services.AddEndpointsApiExplorer();
             builder.Services.AddSwaggerGen();
-
+            builder.Services.AddDbContext<ApplicationDbContext>(options =>
+                options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
             var app = builder.Build();
 
             // Configure the HTTP request pipeline.
