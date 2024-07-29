5c93eaf0f9eb66b64c975ea4dbea264eb7b4e55e
Dilmurod Rozikov <dilmurodrozikovvv@gmail.com>
2024-07-29 10:11:10 +0500

    add openApi support (swagger)

--- ParkingZoneWebApi/Controllers/WeatherForecastController.cs
+++ ParkingZoneWebApi/Controllers/WeatherForecastController.cs
@@ -18,7 +18,7 @@ namespace ParkingZoneWebApi.Controllers
             _logger = logger;
         }
 
-        [HttpGet]
+        [HttpGet(Name = "GetWeatherForecast")]
         public IEnumerable<WeatherForecast> Get()
         {
             return Enumerable.Range(1, 5).Select(index => new WeatherForecast
--- ParkingZoneWebApi/Program.cs
+++ ParkingZoneWebApi/Program.cs
@@ -1,3 +1,4 @@
+
 namespace ParkingZoneWebApi
 {
     public class Program
@@ -9,10 +10,18 @@ namespace ParkingZoneWebApi
             // Add services to the container.
 
             builder.Services.AddControllers();
+            // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+            builder.Services.AddEndpointsApiExplorer();
+            builder.Services.AddSwaggerGen();
 
             var app = builder.Build();
 
             // Configure the HTTP request pipeline.
+            if (app.Environment.IsDevelopment())
+            {
+                app.UseSwagger();
+                app.UseSwaggerUI();
+            }
 
             app.UseHttpsRedirection();
 
