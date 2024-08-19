dd3202d3c408fcdbb7e15a6ef9237a207b28d8b0
Dilmurod Rozikov <dilmurodrozikovvv@gmail.com>
2024-08-19 11:10:08 +0500

    remove an empty line

--- ParkingZoneRESTApi/Services/Interfaces/IReservationService.cs
+++ ParkingZoneRESTApi/Services/Interfaces/IReservationService.cs
@@ -2,9 +2,8 @@
 
 namespace ParkingZoneWebApi.Services.Interfaces
 {
-    public interface IReservationService : IService<Reservation> 
+    public interface IReservationService : IService<Reservation>
     {
-
         public IEnumerable<Reservation> GetReservationsBySlotId(ParkingSlot slot);
     }
 }
